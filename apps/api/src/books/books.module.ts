18453ac7e90eeefde7c2183d0360c83e9883bcbb
nexorqk <pkms2a10@gmail.com>
2026-05-15 18:46:57 +0300

    fix: register GenerationController in BooksModule

--- apps/api/src/books/books.module.ts
+++ apps/api/src/books/books.module.ts
@@ -5,10 +5,11 @@ import { GenerationQueueModule } from '../queues/generation-queue.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { BooksController } from './books.controller';
 import { BooksService } from './books.service';
+import { GenerationController } from './generation.controller';
 
 @Module({
   imports: [AuthModule, PrismaModule, GenerationQueueModule],
-  controllers: [BooksController],
+  controllers: [BooksController, GenerationController],
   providers: [BooksService],
 })
 export class BooksModule {}
